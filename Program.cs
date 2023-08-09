edfd8f1db885994deecbe13a22eb3938d3626a80
Rozikov Dilmurod <2110115@newuzbekistanuniversity.uz>
2023-08-09 20:58:34 +0500

    infra: commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Hello, please enter a number!");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Enter another number!:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Addition: {a+b}");
+
+System.Console.WriteLine($"Subtraction: {a-b}");
+
+System.Console.WriteLine($"Multiplication: {a*b}");
+
+System.Console.WriteLine($"Division: {a/b}");
+
+System.Console.WriteLine($"Remainder: {a%b}");
+
+if(a>b)
+    System.Console.WriteLine("First number is greater than second number.");
+else if(a == b)
+    System.Console.WriteLine("First number is equal to second number.");
+else
+    System.Console.WriteLine("Second number is greater than first number.");
+
